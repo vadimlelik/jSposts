f8c81b554b400dfa4d561e072dad6ba92b303361
vadim.cvirko <promiti90@gmail.com>
2022-03-10 12:02:51 +0300

    render posts

--- src/components/posts.components.js
+++ src/components/posts.components.js
@@ -10,6 +10,35 @@ export class PostsComponents extends Component {
     async onShow() {
         const fBdata = await apiService.fetchPosts()
         const posts = await TransformService.fbObjectToArray(fBdata)
-        console.log(posts);
+        const html = posts.map((post) => renderPosts(post))
+        this.$el.insertAdjacentHTML('afterbegin', html.join(' '))
     }
+    onHide() {
+        this.$el.innerHTML = ' '
+    }
+}
+
+
+
+function renderPosts(post) {
+    const tag = post.type === 'news' ?
+        `<li class="tag tag-blue tag-rounded">Новости</li>` :
+        ` <li class="tag tag-rounded">Заметка</li>`
+
+    return `
+        <div class="panel ">
+          <div class="panel-head">
+            <p class="panel-title">${post.title}</p>
+            <ul class="tags">
+              ${tag}
+            </ul>
+          </div>
+          <div class="panel-body">
+            <p class="multi-line">${post.fulltext}</p>
+          </div>
+          <div class="panel-footer w-panel-footer">
+            <small>${post.date}</small>
+          </div>
+    </div>`
+
 }
